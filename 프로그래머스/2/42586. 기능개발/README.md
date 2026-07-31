98e12d4395822f720111b210e4caca1cbca0346b
Hyemi <78525791+5a6io@users.noreply.github.com>
2026-08-01 00:41:55 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=python3) 
 
 ### 성능 요약
 
-메모리: 88.9 MB, 시간: 21.36 ms
+메모리: 11.5 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 08일 21:28:55
+2026년 08월 01일 00:41:51
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,23 +1,13 @@
+import math
+
 def solution(progresses, speeds):
     answer = []
-    remain = []
-    task = 1
-
-    for i in range(len(speeds)):
-        if (100-progresses[i])%speeds[i] != 0:
-            remain.append(int((100-progresses[i])/speeds[i]) + 1)
-        else:
-            remain.append(int((100-progresses[i])/speeds[i]))
-
-    m = remain[0]
-    for i in range(1, len(remain)):
-        if remain[i] <= m:
-            task+=1
-        else:
-            answer.append(task)
-            task = 1
-            m = remain[i]
-
-    answer.append(task)
-
+    f = 0
+    P = [math.ceil((100-p)/s) for p, s in zip(progresses, speeds)]
+    
+    for i in range(len(P)):
+        if P[i] > P[f]:
+            answer.append(i-f)
+            f = i
+    answer.append(len(P)-f)
     return answer
