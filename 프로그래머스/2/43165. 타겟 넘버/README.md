5e3f67c0da19922f26b80f9add5fcfc7ab613aaa
Hyemi <78525791+5a6io@users.noreply.github.com>
2026-08-04 08:57:53 +0900

    [level 2] Title: 타겟 넘버, Time: 112.43 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 83.3 MB, 시간: 8.36 ms
+메모리: 11.7 MB, 시간: 112.43 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 18일 23:44:58
+2026년 08월 04일 08:57:49
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,11 +1,16 @@
-def dfs(sum, numbers, i, target):
+def dfs(numbers, target, res, i):
     if i == len(numbers):
-        if sum == target:
+        if res == target:
             return 1
         else:
             return 0
-
-    return dfs(sum + numbers[i], numbers, i+1, target)+ dfs(sum - numbers[i], numbers, i+1, target)
+    
+    return dfs(numbers, target, res+numbers[i], i+1) + dfs(numbers, target, res-numbers[i], i+1)
+    
 
 def solution(numbers, target):
-    return dfs(0, numbers, 0, target)
+    answer = 0
+    
+    answer = dfs(numbers, target, 0, 0)
+    
+    return answer
